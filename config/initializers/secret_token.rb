a83509638b69ba4bcf0c80ed986c05052f1bec17
verdasys_user <verdasys_user@RHELC63-x64.localdomain>
2015-02-19 05:58:13 -0500

    ijijdksjdsl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '01e1e0e018f4b3970fffc7d1d0eff6f09de87cd770e902a9f7373fce999dbf292ad45fa44c5d05ab157a72b24dcbed5ced3b87219f2e61d5a3b285e1aba850eb'
